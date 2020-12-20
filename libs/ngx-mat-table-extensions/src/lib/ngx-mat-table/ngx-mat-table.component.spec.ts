41b0e72c510f4a381a546bd45148b82bd99ea8c1
mikelgo <m.gonser@icloud.com>
2020-12-20 17:43:28 +0100

    lib: update test-config

--- libs/ngx-mat-table-extensions/src/lib/ngx-mat-table/ngx-mat-table.component.spec.ts
+++ libs/ngx-mat-table-extensions/src/lib/ngx-mat-table/ngx-mat-table.component.spec.ts
@@ -1,13 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgxMatTableComponent } from './ngx-mat-table.component';
+import { MatTableModule } from '@angular/material/table';
+
+interface TestModel {
+  id: number;
+  name: string;
+}
 
 describe('NgxMatTableComponent', () => {
-  let component: NgxMatTableComponent;
-  let fixture: ComponentFixture<NgxMatTableComponent>;
+  let component: NgxMatTableComponent<TestModel>;
+  let fixture: ComponentFixture<NgxMatTableComponent<any>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [MatTableModule],
       declarations: [ NgxMatTableComponent ]
     })
     .compileComponents();
